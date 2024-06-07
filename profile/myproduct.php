11a8476e8056855e24099f3344a8248a089f5b3b
CarlLevillain <91659706+CarlLevillain@users.noreply.github.com>
2024-06-07 13:39:43 +0200

    front page profile

--- profile/myproduct.php
+++ profile/myproduct.php
@@ -1,1 +1,10 @@
+
 <?php
+if (!empty($products)) {
+    foreach ($products as $product) {
+        displayProduct($product); // Utiliser le template pour afficher chaque produit
+    }
+} else {
+    echo "<p>Aucun produit trouvé.</p>";
+}
+?>
--- profile/profile.php
+++ profile/profile.php
@@ -51,7 +51,7 @@ if (!$pdo) {
 
 <section class="bg-[#15362f] h-24 mb-[30px]">
     <div class="align-middle flex p-4 justify-between w-[80%] m-auto">
-        <img src="../img/logo_overstocks.png">
+        <a href="../index.php"><img src="../img/logo_overstocks.png"></a>
         <?php if ($isLoggedIn): ?>
             <a href="profile/profile.php" class="flex justify-center self-center bg-[#0FFA9C] border-[3px] border-[#0FFA9C] rounded-2xl w-[150px] pt-[6px] pb-[8px]">Mon compte</a>
         <?php else: ?>
@@ -63,30 +63,51 @@ if (!$pdo) {
     </div>
 
 </section>
-<section class="w-[70%]">
+<section class="w-[60%] m-auto">
 
     <!-- Ajouter le texte avec le nom et le prénom de l'utilisateur -->
-    <h2 class="">Bonjour, <?= htmlspecialchars($firstNameOfUser) ?> <?= htmlspecialchars($nameOfUser) ?>!</h2>
+    <div class="flex justify-between items-baseline mb-[30px]">
+        <h2 class="font-geologica text-[55px] text-[#004D42] font-semibold">Bonjour, <?= htmlspecialchars($firstNameOfUser) ?> <?= htmlspecialchars($nameOfUser) ?>!</h2>
+        <a href="logout.php" class="underline">Se déconnecter</a>
+    </div>
+    <h3 class="text-[45px] mb-[50px] font-semibold">Paramètres</h3>
+    <div class="flex gap-[30px] mb-[30px]">
+        <div class="bg-gradient-to-r from-[#A7D8D0] to-[#A7D8D050] rounded-[25px] p-0.5">
+            <a href="infoprofile.php" class="grid content-center w-[400px] h-[100px] justify-center bg-gradient-to-r from-[#3FFBB050] to-[#5EFCBD25] rounded-[25px] text-[#1B453C] font-medium text-[35px] ">Mes informations</a>
+        </div>
+        <div class="bg-gradient-to-r from-[#A7D8D0] to-[#A7D8D050] rounded-[25px] p-0.5">
+            <a href="#" class="grid content-center w-[400px] h-[100px] justify-center bg-gradient-to-r from-[#3FFBB050] to-[#5EFCBD25] rounded-[25px] text-[#1B453C] font-medium text-[35px] ">Mes favoris</a>
+        </div>
+    </div>
+    <div class="flex gap-[30px] mb-[]">
+        <div class="bg-gradient-to-r from-[#A7D8D0] to-[#A7D8D050] rounded-[25px] p-0.5">
+            <a href="#" class="grid content-center w-[400px] h-[100px] justify-center bg-gradient-to-r from-[#3FFBB050] to-[#5EFCBD25] rounded-[25px] text-[#1B453C] font-medium text-[35px] ">Mes achats</a>
+        </div>
+        <div class="bg-gradient-to-r from-[#A7D8D0] to-[#A7D8D050] rounded-[25px] p-0.5">
+            <a href="#" class="grid content-center w-[400px] h-[100px] justify-center bg-gradient-to-r from-[#3FFBB050] to-[#5EFCBD25] rounded-[25px] text-[#1B453C] font-medium text-[35px] ">Besoin d'aide ?</a>
+        </div>
+    </div>
 
-    <h1>Profil de l'Utilisateur</h1>
     <?php if ($typeOfUser === 'professionnel'): ?>
-        <h2>Informations vendeur</h2>
-        <h2>Mes produits</h2>
-        <?php
-        if (!empty($products)) {
-            foreach ($products as $product) {
-                displayProduct($product); // Utiliser le template pour afficher chaque produit
-            }
-        } else {
-            echo "<p>Aucun produit trouvé.</p>";
-        }
-        ?>
-        <a href="myproduct.php">Mes produits</a>
+        <h3 class="text-[45px] mb-[50px] mt-[50px font-semibold">Espace vendeur</h3>
+        <div class="flex gap-[30px] mb-[30px]">
+            <div class="bg-gradient-to-r from-[#A7D8D0] to-[#A7D8D050] rounded-[25px] p-0.5">
+                <a href="#" class="grid content-center w-[400px] h-[100px] justify-center bg-gradient-to-r from-[#3FC3FB75] to-[#3FC3FB25] rounded-[25px] text-[#1B453C] font-medium text-[35px] ">Ajout produit</a>
+            </div>
+            <div class="bg-gradient-to-r from-[#A7D8D0] to-[#A7D8D050] rounded-[25px] p-0.5">
+                <a href="myproduct.php" class="grid content-center w-[400px] h-[100px] justify-center bg-gradient-to-r from-[#3FC3FB75] to-[#3FC3FB25] rounded-[25px] text-[#1B453C] font-medium text-[35px] ">Mes produits</a>
+            </div>
+        </div>
+        <div class="flex gap-[30px]">
+            <div class="bg-gradient-to-r from-[#A7D8D0] to-[#A7D8D050] rounded-[25px] p-0.5">
+                <a href="#" class="grid content-center w-[400px] h-[100px] justify-center bg-gradient-to-r from-[#3FC3FB75] to-[#3FC3FB25] rounded-[25px] text-[#1B453C] font-medium text-[35px] ">Mes ventes</a>
+            </div>
+            <div class="bg-gradient-to-r from-[#A7D8D0] to-[#A7D8D050] rounded-[25px] p-0.5">
+                <a href="#" class="grid content-center w-[400px] h-[100px] justify-center bg-gradient-to-r from-[#3FC3FB75] to-[#3FC3FB25] rounded-[25px] text-[#1B453C] font-medium text-[35px] ">Abonnement</a>
+            </div>
+        </div>
     <?php endif; ?>
 
-    <a href="../index.php">Accueil</a> |
-    <a href="logout.php">Déconnexion</a> |
-    <a href="infoprofile.php">Mes informations</a>
 </section>
 
 <!-- Lien pour Tailwind-->
